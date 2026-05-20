6251b91eba31ab89e273c8774736c863062b012b
Luanna Davi <luannadavidesouza@gmail.com>
2026-05-20 10:08:24 -0300

    ainda tentando concluir a tarefa 03

--- tarefa03/app/urls.py
+++ tarefa03/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name = "index"),
-    path('usuarios',views.usuarios, name="usuarios")
+    path('',views.index,name = "index"),
+    path('usuarios',views.usuario, name="usuarios")
 ]
--- tarefa03/app/views.py
+++ tarefa03/app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-    dados_usuario = {"nome":"Ibrahim"}
+    return render(request,"app/index.html")
+
+
+def usuario(request):
+    dados_usuarios = [{"nome":"Ibrahim","matricula": "5687235678", "idade":"17","Cidade":"SPP"},
+    {"nome":"Lucas Cruz","matricula":"82635735","idade":"18","Cidade":"santa maria"},
+    {"nome":"Lucas carlos","matricula":"75543573","idade":"18","Cidade":"bom jesus"},
+    {"nome":"Ísis","matricula":"4685354988","idade":"19","Cidade":"Serra caiada"},
+    {"nome":"paulo gabriel","matricula":"78483568","idade":"18","Cidade":"SPP"}]
+
+    context = {
+        "usuarios": dados_usuarios,
+    }
+    return render(request, "app/usuarios", context)
