b6e764b1fbdfc4045979d4b368e612a16c4d1a56
Luanna Davi <luannadavidesouza@gmail.com>
2026-07-08 20:34:32 -0300

    tarefa 05

---
+++ tarefa05/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
+    imagem = models.ImageField(upload_to='posts/')
+
+    def __str__(self):
+        return self.titulo
